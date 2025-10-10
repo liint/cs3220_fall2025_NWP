96ebe5db7302cdd1f471579468c5d081200985c5
liint <48611109+liint@users.noreply.github.com>
2025-10-10 14:12:20 -0600

    tests

--- cs3220_2025f-L3-SolvingProblemAgents-UninformedSearch/Lab3Task2/src/environmentClass.py
+++ cs3220_2025f-L3-SolvingProblemAgents-UninformedSearch/Lab3Task2/src/environmentClass.py
@@ -57,8 +57,8 @@ class Environment:
 
   def add_thing(self, thing, location=None):
     #from agentClass import Agent
-    st.write("got into add_thing")
     from src.problemSolvingAgentProgramClass import SimpleProblemSolvingAgentProgram
+    st.write("got into add_thing")
     if thing in self.agents:
       print("Can't add the same agent twice")
     else:
